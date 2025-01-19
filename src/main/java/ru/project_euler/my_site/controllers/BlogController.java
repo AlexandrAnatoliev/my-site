6ac28604f4055945b54e0e33da8c711043792ff2
AlexandrAnatoliev <per-1986@list.ru>
2025-01-19 21:47:41 +0300

    feat: add "add new post" functionality to my web site
    version 0.3.1

--- src/main/java/ru/project_euler/my_site/controllers/BlogController.java
+++ src/main/java/ru/project_euler/my_site/controllers/BlogController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.project_euler.my_site.models.Post;
 import ru.project_euler.my_site.repo.PostRepository;
 
@@ -26,7 +27,9 @@ public class BlogController {
     }
 
     @PostMapping("/blog/add")
-    public String blogPostAdd(Model model){
-        return "";
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
